5fb049c60eb040dc9063392b0cf610358bd80a6f
Bartłomiej Musiał <61922476+bartlomiejmusial@users.noreply.github.com>
2021-01-17 08:47:08 +0100

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,5 +1,4 @@
 import random
-import os
 import time
 
 rock = '''
@@ -31,9 +30,7 @@ scissors = '''
 list1 = [rock, paper, scissors]
 print("Welcome to the rock, paper and scissors game!")
 player = int(input("Choose one option and enter the number\n1 - rock\n2 - paper\n3 - scissors\n"))
-computer = random.randint(1,3)
-
-os.system('clear')
+computer = random.randint(1, 3)
 
 print("✊")
 time.sleep(0.5)
@@ -42,49 +39,48 @@ time.sleep(0.5)
 print("✌️")
 time.sleep(0.5)
 
-os.system('clear')
 if player < 1 or player > 3:
-  print("You entered an invalid number!\nYOU LOSE")
-elif(player == computer):
-  print("\nYou chose:")
-  print(list1[player-1])
-  print("Computer chose:")
-  print(list1[computer-1])
-  print("\nTIE!")
-elif(player == 1 and computer == 2):
-  print("\nYou chose:")
-  print(list1[player-1])
-  print("Computer chose:")
-  print(list1[computer-1])
-  print("\nComputer wins!")
-elif(player == 1 and computer == 3):
-  print("\nPlayer wins!")
-  print("\nYou chose:")
-  print(list1[player-1])
-  print("Computer chose:")
-  print(list1[computer-1])
-  print("\nPlayer wins!")
-elif(player == 2 and computer == 1):
-  print("\nYou chose:")
-  print(list1[player-1])
-  print("Computer chose:")
-  print(list1[computer-1])
-  print("\nPlayer wins!")
-elif(player == 3 and computer == 1):
-  print("\nYou chose:")
-  print(list1[player-1])
-  print("Computer chose:")
-  print(list1[computer-1])
-  print("\nComputer wins!")
-elif(player == 2 and computer == 3):
-  print("\nYou chose:")
-  print(list1[player-1])
-  print("Computer chose:")
-  print(list1[computer-1])
-  print("\nComputer wins!")
-elif(player == 3 and computer == 2):
-  print("\nYou chose:")
-  print(list1[player-1])
-  print("Computer chose:")
-  print(list1[computer-1])
-  print("\nPlayer wins!")
+    print("You entered an invalid number!\nYOU LOSE")
+elif player == computer:
+    print("\nYou chose:")
+    print(list1[player - 1])
+    print("Computer chose:")
+    print(list1[computer - 1])
+    print("\nTIE!")
+elif player == 1 and computer == 2:
+    print("\nYou chose:")
+    print(list1[player - 1])
+    print("Computer chose:")
+    print(list1[computer - 1])
+    print("\nComputer wins!")
+elif player == 1 and computer == 3:
+    print("\nPlayer wins!")
+    print("\nYou chose:")
+    print(list1[player - 1])
+    print("Computer chose:")
+    print(list1[computer - 1])
+    print("\nPlayer wins!")
+elif player == 2 and computer == 1:
+    print("\nYou chose:")
+    print(list1[player - 1])
+    print("Computer chose:")
+    print(list1[computer - 1])
+    print("\nPlayer wins!")
+elif player == 3 and computer == 1:
+    print("\nYou chose:")
+    print(list1[player - 1])
+    print("Computer chose:")
+    print(list1[computer - 1])
+    print("\nComputer wins!")
+elif player == 2 and computer == 3:
+    print("\nYou chose:")
+    print(list1[player - 1])
+    print("Computer chose:")
+    print(list1[computer - 1])
+    print("\nComputer wins!")
+elif player == 3 and computer == 2:
+    print("\nYou chose:")
+    print(list1[player - 1])
+    print("Computer chose:")
+    print(list1[computer - 1])
+    print("\nPlayer wins!")
